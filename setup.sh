9a394bdac9a465c94bba14d7e2ba08e570e8563a
Alec Wong <aw685@cornell.edu>
2020-09-10 18:23:10 -0400

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ ! -d docs ]
+then
+	mkdir docs
+fi
+
+if [ ! -d input ] 
+then
+	mkdir input
+fi
+
+if [ ! -d output ]
+then
+	mkdir output
+fi
+
+if [ ! -d scripts ]
+then
+	mkdir scripts
+fi
+
+if [ ! -d R ]
+then
+	mkdir R
+fi
